685312a3470fbc2b89a73996a75d964cb79be55c
Yurui Zhang <eraclaymore@hotmail.com>
2019-05-24 21:05:18 -0400

    make tests fail

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,37 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import styled from 'styled-components';
+import renderer from 'react-test-renderer';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import { ButtonBase } from '@material-ui/core';
+
+const StyledButton = styled((props) => (
+  <ButtonBase {...props} />
+))`
+  && {
+    background-color: red;
+  }
+`;
+
+const StyledNav = styled(({ active, component, ...rest }) => (
+  <StyledButton component={component} {...rest} />
+))``;
+
+StyledNav.defaultProps = {
+  component: 'a',
+};
+
+it('renders active without crashing', () => {
+  const tree = renderer.create(<StyledNav active>NavItem</StyledNav>).toJSON();
+  expect(tree).toMatchSnapshot();
 });
+
+it('renders disabled without crashing', () => {
+  const tree = renderer.create(<StyledNav disabled>NavItem</StyledNav>).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('renders as button without crashing', () => {
+  const tree = renderer.create(<StyledNav component="button">NavItem</StyledNav>).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
